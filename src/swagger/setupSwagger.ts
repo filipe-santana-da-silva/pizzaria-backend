5021d20a3909a813f41cd7c746ff86e771f812ad
Filipe <filipesantana859@gmail.com>
2025-08-08 15:27:29 -0300

    atualizacoes do swagger

--- src/swagger/setupSwagger.ts
+++ src/swagger/setupSwagger.ts
@@ -1,40 +1,33 @@
 import swaggerUi from 'swagger-ui-express';
-import { Express } from 'express';
 import swaggerJsDoc from 'swagger-jsdoc';
+import { Express } from 'express';
+import fs from 'fs';
+import path from 'path';
+
+const cssPath = path.join(__dirname, '../node_modules/swagger-ui-dist/swagger-ui.css');
+const customCss = fs.readFileSync(cssPath, 'utf8');
 
 const options: swaggerJsDoc.Options = {
-    definition: {
-        openapi: "3.0.0",
-            info: {
-                title: "API Sujeito Pizza",
-                version: "1.0.0",
-                description: "Documentação da API do sistema de pizzaria",
-            },
-            servers: [
-                {
-                    url: "https://pizzaria-backend-pi.vercel.app/",
-                    description: "API em produção"
-                }, {
-                    url: "http://localhost:3333",
-                    description: "Servidor Local",
-                },
-                
-            ],
+  definition: {
+    openapi: "3.0.0",
+    info: {
+    title: "API Sujeito Pizza",
+    version: "1.0.0",
+    description: "Documentação da API do sistema de pizzaria"
     },
-    apis: ["./dist/routes.js", "./dist/swagger/swaggerDocs.js"]
+
+    servers: [
+      { url: "https://pizzaria-backend-pi.vercel.app/", description: "Produção" },
+      { url: "http://localhost:3333", description: "Local" }
+    ]
+  },
+  apis: ["./src/routes.ts", "./src/swagger/swaggerDocs.ts"]
 };
 
 const swaggerSpec = swaggerJsDoc(options);
 
 export function setupSwagger(app: Express) {
-   app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec, {
-    customCssUrl: 'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
-    customJs: [
-        'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.min.js',
-        'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.js'
-    ]
-    }));
-
-
-
+  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec, {
+    customCss
+  }));
 }
